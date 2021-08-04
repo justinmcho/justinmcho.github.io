a9c5e080cde4ced47c126a8d6141341746da72da
Justin Cho <justinmcho.k@gmail.com>
2021-08-05 00:07:41 +0900

    hash router

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Experience from "../views/Experience.vue";
 import Blog from "../views/Blog.vue";
@@ -29,7 +29,8 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  mode: 'hash',
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
   scrollBehavior(to, from, savedPosition) {
     return { top: 0 };
